1c0f2c3429c31166b1146deaff7460bc65c1ebaf
Kate <eerteter@inbox.ru>
2023-01-30 22:14:07 +0300

    first review

--- scripts/script.js
+++ scripts/script.js
@@ -1,29 +1,121 @@
-let popup = document.querySelector('.popup');
-let popupOpen = document.querySelector('.profile__edit-button');
-let popupClose = document.querySelector('.popup__toggle');
-let formElement = document.querySelector('.popup__form');
-let nameInput = document.querySelector('.popup__item_type_name');
-let jobInput = document.querySelector('.popup__item_type_profession');
-let nameProfile = document.querySelector('.profile__name');
-let jobProfile = document.querySelector('.profile__caption');
+// popups
+const popupProfile = document.querySelector('.popup_type_profile');
+const popupCards = document.querySelector('.popup_type_cards');
+const popupView = document.querySelector('.popup_type_view');
 
-function openPopup() {
-    nameInput.value = nameProfile.textContent; 
-    jobInput.value = jobProfile.textContent; 
-    popup.classList.add('popup_opened');
+//popups close buttons
+const popupCloseProfile = popupProfile.querySelector('.popup__toggle');
+const popupCloseCards = popupCards.querySelector('.popup__toggle');
+const popupCloseView = popupView.querySelector('.popup__toggle');
+
+// popups open button
+const popupOpenProfile = document.querySelector('.profile__edit-button');
+const popupOpenCards = document.querySelector('.profile__add-button');
+const popupOpenView = document.querySelector('.popup__img');
+
+// кнопки сабмитов
+const popupSubmitProfile = popupProfile.querySelector('.popup__button');
+const popupSubmitCards = popupCards.querySelector('.popup__button');
+
+/* */
+const nameInput = document.querySelector('.popup__item_type_name');
+const jobInput = document.querySelector('.popup__item_type_profession');
+const placeInput = document.querySelector('.popup__item_type_place');
+const linkInput = document.querySelector('.popup__item_type_link');
+const nameProfile = document.querySelector('.profile__name');
+const jobProfile = document.querySelector('.profile__caption');
+const cardList = document.querySelector('.grid-elements__list');
+
+/* подгрузка карточки из массива*/
+
+function renderCards() {
+  initialCards.forEach((item) => {
+    createCard(item);
+  });
+}
+renderCards();
+
+/* визуализация карточки на сайте с лайками и удалением*/
+
+function createCard(item) {
+  const cardTemplate = document.querySelector('.card-template').content;
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+  cardElement.querySelector('.card__name').textContent = item.name;
+  cardElement.querySelector('.card__img').src = item.link;
+  cardElement.querySelector('.card__img').alt = item.name;
+  cardList.prepend(cardElement);
+
+  const likeButton = document.querySelector('.card__like-button');
+  likeButton.addEventListener('click', () => {
+    likeButton.classList.toggle('card__like-button_active');
+  })
+
+  const trashButton = document.querySelector('.card__trash-button');
+  trashButton.addEventListener('click', () => {
+    cardElement.remove();
+  })
+
+  const image = document.querySelector('.card__img');
+  const popupImg = document.querySelector('.popup__img');
+  const popupImgName = document.querySelector('.popup__caption');
+  image.addEventListener('click', () => {
+    popupImg.src = item.link;
+    popupImgName.textContent = item.name;
+    popupImgName.alt = item.name;
+    togglePopup(popupView);
+
+  })
+  }
+
+//открытие попапов
+const togglePopup = function (popup) {
+  popup.classList.toggle('popup_opened');
 }
 
-function closePopup() {
-    popup.classList.remove('popup_opened');
-}
+popupOpenProfile.addEventListener('click', function () {
+  nameInput.value = nameProfile.textContent;
+  jobInput.value = jobProfile.textContent;
+  togglePopup(popupProfile);
+})
 
-function handleFormSubmit (evt) {
-    evt.preventDefault();
-    nameProfile.textContent = nameInput.value;
-    jobProfile.textContent = jobInput.value;
-    closePopup();
-}
+popupOpenCards.addEventListener('click', function () {
+  togglePopup(popupCards);
+})
+popupOpenView.addEventListener('click', function () {
+  togglePopup(popupView);
+})
 
-popupOpen.addEventListener('click', openPopup);
-popupClose.addEventListener('click', closePopup);
-formElement.addEventListener('submit', handleFormSubmit); 
+//закрытие попапов
+popupCloseProfile.addEventListener('click', function () {
+  togglePopup(popupProfile);
+})
+popupCloseCards.addEventListener('click', function () {
+  togglePopup(popupCards);
+})
+
+popupCloseView.addEventListener('click', function () {
+  togglePopup(popupView);
+})
+
+/* сабмит в попап профиля */
+
+const submitPopupProfile = popupSubmitProfile.addEventListener('click', (evt) => {
+  evt.preventDefault();
+  nameProfile.textContent = nameInput.value;
+  jobProfile.textContent = jobInput.value;
+  togglePopup(popupProfile);
+
+})
+
+/* сабмит в карточки */
+
+const submitPopupCards = popupSubmitCards.addEventListener('click', (evt) => {
+  evt.preventDefault();
+  const title = placeInput.value;
+  const link = linkInput.value;
+  const card = createCard({name: title, link: link});
+  placeInput.value = '';
+  linkInput.value = '';
+  togglePopup(popupCards);
+
+ })
---
+++ scripts/script2.js
@@ -0,0 +1,26 @@
+const initialCards = [
+  {
+    name: 'Индия',
+    link: 'https://images.unsplash.com/photo-1514222134-b57cbb8ce073?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80'
+  },
+  {
+    name: 'Тайланд',
+    link: 'https://images.unsplash.com/photo-1504214208698-ea1916a2195a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80'
+  },
+  {
+    name: 'Япония',
+    link: 'https://images.unsplash.com/photo-1545569341-9eb8b30979d9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80'
+  },
+  {
+    name: 'Шри-Ланка',
+    link: 'https://images.unsplash.com/photo-1638434369239-a4f6cd1dbbb0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80'
+  },
+  {
+    name: 'Корея',
+    link: 'https://images.unsplash.com/photo-1603852452515-2dc92bd9c6f1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=928&q=80'
+  },
+  {
+    name: 'Непал',
+    link: 'https://images.unsplash.com/photo-1580424917967-a8867a6e676e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80'
+  }
+];
